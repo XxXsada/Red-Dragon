79251e1ede6ac517b72242e46e2f657ae176f715
XxXsada <108647304+XxXsada@users.noreply.github.com>
2022-07-04 12:43:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,7 +20,9 @@ global.APIKeys = {
 //customize settings\\
 
 global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+My Creator is Poojitha Sadamina
+
+😎` //Costomize Alive Message (type your message in `` )
 
 global.owner = ['+380944814219'] //Owner number in aive msg
 global.premium = ['+380944814219'] //Owner Number info
